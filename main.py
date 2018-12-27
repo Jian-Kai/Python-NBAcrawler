36380c9a912360592f4a02e644e820e4a2eeee87
tony14256 <tony14256@gmail.com>
2018-12-27 09:15:46 +0800

    save data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import get_game
 import get_gamebox
+import json
 
 class GameBox:
     starts = ''
@@ -9,16 +10,25 @@ class GameMatch:
     HomeTeam = ''
     AwayTeam = ''
     GameStatus = ''
+gameID = '401071167'
+gameinfo = get_game.get_game(gameID)
+gamebox = get_gamebox.gamebox(gameID)
 
-gameinfo = get_game.get_game('401070837')
-gamebox = get_gamebox.gamebox('401070837')
-
+#game scroe
 Game = GameMatch()
 Game.AwayTeam = gameinfo[0]
 Game.HomeTeam = gameinfo[1]
 Game.GameStatus = gameinfo[2]
 
+#game box
 GmaeBox = GameBox()
 GameBox.starts = [gamebox[0], gamebox[1]]
 GameBox.bench = [gamebox[2], gamebox[3]]
 
+#save data
+Gamescorejson = {}
+Gamescorejson['AwayTeam'] = Game.AwayTeam
+Gamescorejson['HomeTeam'] = Game.HomeTeam
+Gamescorejson['GameStatus'] = Game.GameStatus
+with open('data.json', 'w') as outfile:  
+    json.dump(Gamescorejson, outfile)
